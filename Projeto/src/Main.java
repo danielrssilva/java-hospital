dc53ec45bc97368c086f6e97fa15c849549cdc14
Daniel Ribeiro <danielribeirosoaressilva@gmail.com>
2019-04-03 01:01:28 -0300

    Adicionando os forms

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -1,3 +1,4 @@
+import Forms.Login;
 import JavaBeans.*;
 import ClassesDao.*;
 import conexao.ConnectionFactory;
@@ -5,17 +6,7 @@ import conexao.ConnectionFactory;
 public class Main {
 
     public static void main(String[] args) {
-        Endereco endereco = new Endereco();
-        EnderecoDao endDAo = new EnderecoDao();
-
-        endereco.setEstado("sp");
-        endereco.setCidade("Hortolandia");
-        endereco.setBairro("Remanso Campineiro");
-        endereco.setLogradouro("rua:42");
-        endereco.setNumero("234");
-        endereco.setBloco("00");
-
-        endDAo.adiciona(endereco);
-
+        Login login = new Login();
+        login.montarFrame();
     }
 }
