fba1a1bb9ce7147eb8e94262d67cef9059fe4777
Daniel Ribeiro <danielribeirosoaressilva@gmail.com>
2019-04-04 20:54:04 -0300

    Alterando arquivo do banco

---
+++ hospital.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2019 at 10:13 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.27
+CREATE DATABASE hospital;
+USE hospital;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diagnostico`
+--
+
+CREATE TABLE `diagnostico` (
+  `idDiagnostico` int(11) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `especialidade` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `idEndereco` int(11) NOT NULL,
+  `estado` varchar(40) NOT NULL,
+  `cidade` varchar(40) NOT NULL,
+  `bairro` varchar(40) NOT NULL,
+  `logradouro` varchar(40) NOT NULL,
+  `numero` varchar(4) NOT NULL,
+  `bloco` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enfermeiro`
+--
+
+CREATE TABLE `enfermeiro` (
+  `matricula` varchar(9) NOT NULL,
+  `coren` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `matricula` varchar(9) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `dataNascimento` date,
+  `cargo` varchar(30) NOT NULL,
+  `horaInicio` time NOT NULL,
+  `horaFim` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medico`
+--
+
+CREATE TABLE `medico` (
+  `matricula` varchar(9) NOT NULL,
+  `crm` varchar(20) NOT NULL,
+  `especialidade` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `idPaciente` int(11) NOT NULL,
+  `nome` int(11) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `dataNascimento` int(14),
+  `idEndereco` int(11) NOT NULL,
+  `idDiagnostico` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `diagnostico`
+--
+ALTER TABLE `diagnostico`
+  ADD PRIMARY KEY (`idDiagnostico`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`idEndereco`);
+
+--
+-- Indexes for table `enfermeiro`
+--
+ALTER TABLE `enfermeiro`
+  ADD PRIMARY KEY (`matricula`),
+  ADD UNIQUE KEY `coren` (`coren`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`matricula`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- Indexes for table `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`matricula`),
+  ADD UNIQUE KEY `crm` (`crm`);
+
+--
+-- Indexes for table `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`idPaciente`),
+  ADD UNIQUE KEY `cpf` (`cpf`),
+  ADD KEY `idDiagnostico` (`idDiagnostico`),
+  ADD KEY `idEndereco` (`idEndereco`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `diagnostico`
+--
+ALTER TABLE `diagnostico`
+  MODIFY `idDiagnostico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `idEndereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enfermeiro`
+--
+ALTER TABLE `enfermeiro`
+  ADD CONSTRAINT `enfermeiro_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `funcionario` (`matricula`);
+
+--
+-- Constraints for table `medico`
+--
+ALTER TABLE `medico`
+  ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `funcionario` (`matricula`);
+
+--
+-- Constraints for table `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`idDiagnostico`) REFERENCES `diagnostico` (`idDiagnostico`),
+  ADD CONSTRAINT `paciente_ibfk_2` FOREIGN KEY (`idEndereco`) REFERENCES `endereco` (`idEndereco`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
